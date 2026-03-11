9a5493830f2c13fd9bb78a00ef2bea278ef26fe0
kavyadevops4 <smvkavya@gmail.com>
2026-03-11 11:15:27 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,25 +1,18 @@
 package com.mycompany.app;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest
-{
     @Test
-    public void testAppConstructor() {
-        App app1 = new App();
-        App app2 = new App();
-        assertEquals(app1.getMessage(), app2.getMessage());
+    void testSayHello() {
+        App app = new App();
+        assertEquals("Hello from Maven Project!", app.sayHello());
     }
 
     @Test
-    public void testAppMessage()
-    {
-        App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+    void testMainMethod() {
+        App.main(new String[]{});
     }
 }
